dc2248124e8f667ff0ae4d3a8a26aef01df34b3d
nk-randers <nivethan.kandiah@randers.dk>
2025-05-06 12:55:51 +0200

    added util for Azure AD data

---
+++ src/utils/azure_ad_data.py
@@ -0,0 +1,14 @@
+from utils.api_requests import APIClient
+import streamlit as st
+from utils.config import API_SERVICE_URL
+
+
+@st.cache_data
+def get_user_locations():
+    try:
+        client = APIClient(base_url=API_SERVICE_URL)
+        response = client.make_request(path="api/azure/user-department", method="get")
+        return response.get("user_department", [])
+    except Exception as e:
+        st.error(f"Kunne ikke hente Azure AD data: {e}")
+        return []
